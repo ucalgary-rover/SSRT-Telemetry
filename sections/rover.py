cfd883330786a63571258bc0db296ef620a07772
Hasan Salhi <hasan.salhi2006@gmail.com>
2026-02-07 13:40:19 -0700

    Added the images to the rover health section of the interface. Still need to do formatting and proper aligning

--- sections/rover.py
+++ sections/rover.py
@@ -5,15 +5,22 @@ def display():
     pitch, roll, wheels, arm, power = st.columns(5)
 
     with pitch:
+        with st.container(key="rover-pitch"):
+            st.image("assets/rover-side-view.png", use_container_width=False)
         st.text("Pitch")
 
     with roll:
+        with st.container(key="rover-roll"):
+            st.image("assets/rover-front-view.png", use_container_width=False)
         st.text("Roll")
 
     with wheels:
+        with st.container(key="rover-wheels"):
+            st.image("assets/rover-wheels.png", use_container_width=False)
         st.text("Wheels")
 
     with arm:
+        st.image("assets/rover-arm.png", use_container_width=False)
         st.text("Arm")
 
     with power:
