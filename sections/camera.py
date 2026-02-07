981c5f253e97c6afddf977b467a39f1fc2ed8db0
l-qu <132942951+ljquong@users.noreply.github.com>
2026-02-06 17:44:44 -0700

    Added popout button to camera page

--- sections/camera.py
+++ sections/camera.py
@@ -4,10 +4,11 @@ from src.components import horizontal_divider
 
 # placeholder images for now
 CAMERA_FEEDS = {
-    "Camera 1" : "assets/cameras/placeholder-1.jpg",
-    "Camera 2" : "assets/cameras/placeholder-2.jpg",
-    "Camera 3" : "assets/cameras/placeholder-3.jpg",
+    "Camera 1": "assets/cameras/placeholder-1.jpg",
+    "Camera 2": "assets/cameras/placeholder-2.jpg",
+    "Camera 3": "assets/cameras/placeholder-3.jpg",
 }
+
 
 def display():
     with st.container(key="camera-container"):
@@ -16,13 +17,20 @@ def display():
         # dropdown menu, does not show label 'camera'
         with dropdown_select:
             # st.text("Camera dropdown select")
-            selected_camera = st.selectbox("Camera", options=list(CAMERA_FEEDS.keys()), label_visibility="collapsed", key="telemetry_camera_select",)
+            selected_camera = st.selectbox(
+                "Camera",
+                options=list(CAMERA_FEEDS.keys()),
+                label_visibility="collapsed",
+                key="telemetry_camera_select",
+            )
 
         with popout:
-            st.text("Popout")
+            if st.button("↗", width="stretch"):
+                st.switch_page("pages/Camera_Page.py")
+            # st.text("Popout")
 
         horizontal_divider()
-        st.image(CAMERA_FEEDS[selected_camera], width='stretch')
+        st.image(CAMERA_FEEDS[selected_camera], width="stretch")
         # box changes size depending on image, maybe fix w/ css aspect ratio?
 
         # st.text("Camera Display")
