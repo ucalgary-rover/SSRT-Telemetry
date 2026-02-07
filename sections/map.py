ca89be09841ab37570507e0fe27c27094e93d472
fabf28 <ifavour228@gmail.com>
2026-02-07 11:51:05 -0700

    add map and placeholder values

--- sections/map.py
+++ sections/map.py
@@ -1,28 +1,40 @@
 import streamlit as st
+import pandas as pd
 
 from src.components import horizontal_divider
 
 
 def display():
+    data = {"Longitude": 6.44,
+            "Latitude": 7.44,
+            "Speed": 8.33,
+            "POI": 53234,
+            "Heading": "random"}
+    
+    df = pd.DataFrame({
+        "lat": [37.76, 37.77, 37.78],
+        "lon": [-122.4, -122.41, -122.42]
+    })
     with st.container(key="map-container"):
         map, status = st.columns([0.75, 0.25])
 
         # add the actual map tile here and draw the path on top
         with map:
-            st.text("Map")
+            st.map(df)
 
         # put the status information to the side
         with status:
-            st.text("Heading")
-            st.text("Speed")
-            st.text("Add POI Button")
+            st.image("https://www.shutterstock.com/image-vector/architectural-north-arrow-compass-outline-260nw-2635030447.jpg")
+            with st.empty().container():
+                st.write(f"SPEED: {data['Speed']}")
+                st.text("Add POI Button")
 
         horizontal_divider()
 
         lat, long = st.columns(2)
 
         with lat:
-            st.text("Latitude")
+            st.write(f"LAT: {data['Latitude']}")
 
         with long:
-            st.text("Longitude")
+            st.write(f"LONG: {data['Longitude']}")
