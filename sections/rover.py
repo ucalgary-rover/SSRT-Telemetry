fcab6a6ba1b5a172dd831b6f5fe708478530ae59
Hasan Salhi <hasan.salhi2006@gmail.com>
2026-02-12 13:01:19 -0700

    Expanded CSS styling for better organization on interface

--- sections/rover.py
+++ sections/rover.py
@@ -2,28 +2,32 @@ import streamlit as st
 
 
 def display():
-    pitch, roll, wheels, arm, power = st.columns(5)
+    pitch, roll, wheels, arm, power = st.columns(5, vertical_alignment="center")
 
     with pitch:
         with st.container(key="rover-pitch"):
             st.image("assets/rover-side-view.png", use_container_width=False)
-        st.text("Pitch")
+        with st.container(key="rover-pitch-text"):
+            st.markdown("Pitch")
 
     with roll:
         with st.container(key="rover-roll"):
-            st.image("assets/rover-front-view.png", use_container_width=False)
-        st.text("Roll")
+            st.image("assets/rover-front-view.png", use_container_width=False)        
+        with st.container(key="rover-roll-text"):
+            st.markdown("Roll")
 
     with wheels:
         with st.container(key="rover-wheels"):
             st.image("assets/rover-wheels.png", use_container_width=False)
-        st.text("Wheels")
+        with st.container(key="rover-wheels-text"):
+            st.markdown("Wheels")
 
     with arm:
-        st.image("assets/rover-arm.png", use_container_width=False)
-        st.text("Arm")
+        with st.container(key="rover-arm"):
+            st.image("assets/rover-arm.png", use_container_width=False)
+        with st.container(key="rover-arm-text"):
+            st.markdown("Arm")
 
     with power:
         st.text("Battery Temperature")
-        st.text("Power")
         st.text("Power")
