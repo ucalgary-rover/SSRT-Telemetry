20b9e532d9e5e8c68b96285598ddb27d1b26c70d
Hasan Salhi <hasan.salhi2006@gmail.com>
2026-02-14 11:38:08 -0700

    Added image rotation (pitch and roll) and display of important values

--- sections/rover.py
+++ sections/rover.py
@@ -1,33 +1,52 @@
 import streamlit as st
 
+pitch = -15
+roll = 24
+battery_temp = 42
+power = 69
 
 def display():
-    pitch, roll, wheels, arm, power = st.columns(5, vertical_alignment="center")
+    st.markdown(f"""
+        <style>
+        :root {{
+            --pitch-deg: {pitch}deg;
+            --roll-deg: {roll}deg;
+        }}
+        </style>
+    """, unsafe_allow_html=True)
+    
+    pitch_col, roll_col, wheels_col, arm_col, power_col = st.columns(5, vertical_alignment="top")
 
-    with pitch:
+    with pitch_col:
         with st.container(key="rover-pitch"):
-            st.image("assets/rover-side-view.png", use_container_width=False)
+            st.image("assets/rover-side-view.png", width='content')
         with st.container(key="rover-pitch-text"):
-            st.markdown("Pitch")
+            st.markdown("Pitch: %0.2f°" % pitch)
 
-    with roll:
+    with roll_col:
         with st.container(key="rover-roll"):
-            st.image("assets/rover-front-view.png", use_container_width=False)        
+            st.image("assets/rover-front-view.png", width='content')        
         with st.container(key="rover-roll-text"):
-            st.markdown("Roll")
+            st.markdown("Roll: %0.2f°" % roll)
 
-    with wheels:
+    with wheels_col:
         with st.container(key="rover-wheels"):
-            st.image("assets/rover-wheels.png", use_container_width=False)
+            st.image("assets/rover-wheels.png", width='content')
         with st.container(key="rover-wheels-text"):
             st.markdown("Wheels")
 
-    with arm:
+    with arm_col:
         with st.container(key="rover-arm"):
-            st.image("assets/rover-arm.png", use_container_width=False)
+            st.image("assets/rover-arm.png", width='content')
         with st.container(key="rover-arm-text"):
             st.markdown("Arm")
 
-    with power:
-        st.text("Battery Temperature")
-        st.text("Power")
+    with power_col:
+        with st.container(key="battery-temp-text"):
+            st.markdown("Battery Temperature")
+        with st.container(key="battery-temp"):
+            st.markdown("%0.2f°C" % battery_temp)
+        with st.container(key="power-text"):
+            st.markdown("Power")
+        with st.container(key="power"):    
+            st.markdown("%0.2f%%" % power)
