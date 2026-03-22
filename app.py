a1df03efbe9bc72d48dc22052a0ecbf2d03c6d27
Dani Jourdain <danijourdain@icloud.com>
2026-03-21 20:54:20 -0600

    subscribe to 2 temperature topics

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ setup_logging()
 import streamlit as st
 from mqtt.subscriber import MQTTSubscriber
 from utils.read_env import read_env_variable
+from state.read_latest_from_queue import latest_values
 
 REFRESH_DELAY = float(read_env_variable("REFRESH_DELAY"))
+SAMPLE_TOPIC = read_env_variable("SAMPLE_TOPIC")
+SAMPLE_TOPIC2 = read_env_variable("SAMPLE_TOPIC2")
 
 
 @st.cache_resource
@@ -16,13 +19,18 @@ def init_mqtt():
 
 @st.fragment(run_every=REFRESH_DELAY)
 def temperature_section():
-    from state.read_latest_from_queue import latest_values
+    value, updated = latest_values[SAMPLE_TOPIC].get_if_updated()
+    if updated:
+        st.session_state.latest_temp = value
+    st.metric("Temperature", st.session_state.get("latest_temp", "Waiting..."))
 
-    value, updated = latest_values[read_env_variable("SAMPLE_TOPIC")].get_if_updated()
-    if not updated:
-        return
-    st.session_state.latest_temp = value
-    st.metric("Temperature", st.session_state.latest_temp)
+
+@st.fragment(run_every=REFRESH_DELAY)
+def temperature_section2():
+    value, updated = latest_values[SAMPLE_TOPIC2].get_if_updated()
+    if updated:
+        st.session_state.latest_temp2 = value
+    st.metric("Temperature 2", st.session_state.get("latest_temp2", "Waiting..."))
 
 
 def main():
@@ -36,8 +44,10 @@ def main():
 
     if "latest_temp" not in st.session_state:
         st.session_state.latest_temp = "Waiting..."
+        st.session_state.latest_temp2 = "Waiting..."
 
     temperature_section()
+    temperature_section2()
 
 
 if __name__ == "__main__":
